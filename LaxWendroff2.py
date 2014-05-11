11829382a317cc732d739de10aa628897baa6c7f
Noe Cardarelli <mncardarelli@gmail.com>
2014-05-11 18:29:03 -0300

    LaxWendroff script con ejemplo (falta tratar condiciones de borde)

--- LaxWendroff2.py
+++ LaxWendroff2.py
@@ -4,71 +4,65 @@ Created on Mon Apr 28 10:12:38 2014
 
 @author: Noelia
 """
-from numpy import *
-#from sympy import *
 
-#p,u=symbols('p,u')
-#f=p**0.5
-#A = matrix([[-u, f.diff(p)],[-p, -u]])
-#Ap=lambdify([u,p],A.diff(p),'numpy')
-#Au=lambdify([u,p],A.diff(u),'numpy')
-#A=array([[lambdify([u, ro], A[0,0], "numpy"), lambdify([u, ro], A[0,1], "numpy")], \
-#              [lambdify([u, ro], A[1,0], "numpy"), lambdify([u, ro], A[1,1], "numpy")]])
-#A=array([[lambda u:-u, lambda ro: ro**0.5], [lambda ro: -ro, lambda u:-u]])
-#Au=array([[-1, 0], ])
+import numpy as np
+
 dx=.1
 dt=.1
 tmax=1
 RoValorInic=lambda x:x
 uValorInic=lambda x:x
-A=lambda u, ro:array([[-u, ro**2], [-ro, -u]])
-Au=lambda u, ro:array([[-ones(shape(ro)), zeros(shape(ro))], [zeros(shape(ro))\
-, -ones(shape(ro))]])
-Aro=lambda u, ro:array([[zeros(shape(ro)), 2*ro], [-ones(shape(ro)), \
-zeros(shape(ro))]])
+A=lambda u, ro:np.array([[-u, ro**2], [-ro, -u]])
+Au=lambda u, ro:np.array([[-np.ones(np.shape(ro)), np.zeros(np.shape(ro))], \
+[np.zeros(np.shape(ro)), -np.ones(np.shape(ro))]])
+Aro=lambda u, ro:np.array([[np.zeros(np.shape(ro)), 2*ro], [-np.ones(np.shape(ro)), \
+np.zeros(np.shape(ro))]])
 
 
 #def esquemalaxwendroff(dx,tmax,dt,RoValorInic,uValorInic,Ap,Au):
 """
 RoValorInic,uValorInic son funciones de dos variables en los reales
-A es una array con las funciones de variables u y ro escritas como cadena
+A es una np.array con las funcinp.ones de variables u y ro escritas como cadena
 """    
 
 
-nFila=int(1/dx)
-x=linspace(0,1,nFila+1)
-tt=linspace(0,1,int(tmax/dt)+1)
-V=np.zeros((size(tt),2,size(x)))
-V[0,0,:]=uValorInic(x)
-V[0,1,:]=RoValorInic(x)
-#vect=np.zeros([size(x), 2, 1])
-V0= V[0,:,:]
-AA=A(V[0,0,:],V[0,1,:])
-AAu=Au(V[0,0,:],V[0,1,:])
-AAro=Aro(V[0,0,:],V[0,1,:])
-Vx=zeros(shape(V0[:,1:-1]))
-Vxx=Vx
-for t in range(0,int(tmax/dt)):
-    Vx=(V0[:,2:nFila+1]-V0[:,0:nFila-1])/(2*dx)
-    Vxx=(V0[:,2:nFila+1]-2*V0[:,1:nFila]+V0[:,0:nFila-1])/(dx**2)
-        
-    Vt[1:nFila,:]=dot(AA,Vx)
+nPuntos=int(1/dx)+1
+nPasos=int(tmax/dt)+1
+x=np.linspace(0,1,nPuntos)
+#tt=linspace(0,1,int(tmax/dt)+1)
+V=np.zeros((nPasos,2,nPuntos)) #Inicializo V
+V[0,0,:]=uValorInic(x) #Cálculo primer nivel para la variable u
+V[0,1,:]=RoValorInic(x) #Cálculo primer nivel para la variable ro
+V0= V[0,:,:] #Guardo el primer nivel para construir el siguiente
+AA=A(V[0,0,1:-1],V[0,1,1:-1]) #Evalúo A en u y ro en el tiempo t=0
+AAu=Au(V[0,0,1:-1],V[0,1,1:-1]) ##Evalúo Au en u y ro en el tiempo t=0
+AAro=Aro(V[0,0,1:-1],V[0,1,1:-1]) #Evalúo Aro en u y ro en el tiempo t=0
+Vx=Vxx=Vt=Vxt=Vtt=np.zeros(np.shape(V0[:,1:-1])) #Inicializo Vx, Vxx, Vt, Vxt y Vtt
+AAx=AAt=np.zeros(np.shape(AA)) #Inicializo AAx y AAt
+cte1=2*dx
+cte2=dx**2
+cte3=cte2/2
+for t in range(1,nPasos):
+    Vx=(V0[:,2:]-V0[:,0:-2])/cte1 #Cálculo de Vx por diferencia central
+    Vxx=(V0[:,2:]-2*V0[:,1:-1]+V0[:,0:-2])/cte2 #Cálculo de Vxx por diferencia central
     
-    vect[:,0,0]=Vx[0,:,0]
-    vect[:,1,0]=Vx[0,:,1]
-    Mx=Mu*vect[:,0,0]+Mro*vect[:,1,0]
-    vect[:,0,0]=Vt[0,:,0]
-    vect[:,1,0]=Vt[0,:,1]
-    Mt=Mu*vect[:,0,0]+Mro*vect[:,1,0]
+    Vt=sum(AA*Vx,1) #Cálculo de Vt por medio de la ecuación diferencial donde \
+    # sum(AA*Vx,1) es un producto matricial
     
-    Vxt[t,1:nFila,:]=Mx[1:nFila,:,:]*Vx[t,1:nFila,:]+\
-        M[1:nFila,:,:]*Vx[t,1:nFila,:]
-    Vtt[t,1:nFila,:]=Mt[1:nFila,:,:]*Vx[t,1:nFila,:]+\
-        M[1:nFila,:,:]*Vxt[t,1:nFila,:]
-    V[t+1,1:nFila,:]=V[t,1:nFila,:]+Vt[t,1:nFila,:]*dt+\
-        Vtt[t,1:nFila,:]*(dt**2)/2
+    AAx=AAu*Vx[0]+AAro*Vx[1]
+    AAt=AAu*Vt[0]+AAro*Vt[1]
+    
+    Vxt=sum(AAx*Vx,1)+sum(AA*Vxx,1) #Cálculo de Vxt por medio de derivar la \
+    # ecuación diferencial respecto de x
+    Vtt=sum(AAt*Vx,1)+sum(AA*Vxt,1) #Cálculo de Vtt por medio de derivar la \
+    # ecuación diferencial respecto de t
+    
+    V[t,:,1:-1]=V0[:,1:-1]+Vt*dt+Vtt*cte3 #Cálculo de la solución por Taylor \
+    # salvo en los bordes
         
-    V0= V[t+1,:,:]
-    AA=A(V[t+1,:,0],V[t+1,:,1])
-    AAu=Au(V[t+1,:,0],V[t+1,:,1])
-    AAro=Aro(V[t+1,:,0],V[t+1,:,1])
+    V0= V[t,:,:]  #Guardo el nivel contruido para construir el siguiente
+    AA=A(V[t,0,1:-1],V[t,1,1:-1]) #Evalúo A en u y ro en el tiempo t
+    AAu=Au(V[t,0,1:-1],V[t,1,1:-1]) #Evalúo Au en u y ro en el tiempo t
+    AAro=Aro(V[t,0,1:-1],V[t,1,1:-1]) #Evalúo Aro en u y ro en el tiempo t
+
+""" faltan las condiciones de borde """
